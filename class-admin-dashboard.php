6c4253d764de8e5776a1f905365c8c5cec055377
herrschuessler <schreib@herrschuessler.de>
2020-01-09 14:18:37 +0100

    Display active support contract status

--- class-admin-dashboard.php
+++ class-admin-dashboard.php
@@ -2,9 +2,11 @@
 /**
  * Customize WP Admin Dashboard with Support Info
  *
- * @package     Mo\Support
- * @author      MONTAGMORGENS GmbH
- * @copyright   2020 MONTAGMORGENS GmbH
+ * @category   Plugin
+ * @package    Mo\Support
+ * @author     Christoph Schüßler <schuessler@montagmorgens.com>
+ * @license    https://www.gnu.org/licenses/gpl-2.0.txt GNU/GPLv2
+ * @since      1.0.0
  *
  * @wordpress-plugin
  * Plugin Name: MONTAGMORGENS Support
@@ -13,6 +15,7 @@
  * Author:      MONTAGMORGENS GmbH
  * Author URI:  https://www.montagmorgens.com/
  * License:     GNU General Public License v.2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: mo-support
  * GitHub Plugin URI: montagmorgens/mo-support
  */
@@ -27,6 +30,11 @@ if ( ! is_admin() ) {
 	return;
 }
 
+// Define absolute path to plugin root.
+if ( ! defined( 'Mo\Support\PLUGIN_PATH' ) ) {
+	define( 'Mo\Support\PLUGIN_PATH', wp_normalize_path( plugin_dir_path( __FILE__ ) ) );
+}
+
 // Init plugin instance.
 \add_action( 'plugins_loaded', '\Mo\Support\Admin_Dashboard::get_instance' );
 
@@ -36,6 +44,8 @@ if ( ! is_admin() ) {
  * @var object|null $instance The plugin singleton.
  */
 final class Admin_Dashboard {
+
+	const PLUGIN_VERSION = '1.0.0';
 
 	/**
 	 * The plugin singleton.
@@ -62,8 +72,16 @@ final class Admin_Dashboard {
 	private function __construct() {
 
 		// Add action hooks.
-		\add_action( 'admin_init', array( $this, 'remove_dashboard_widgets' ) );
-		\add_action( 'wp_dashboard_setup', array( $this, 'mo_dashboard' ) );
+		\add_action( 'admin_init', [ $this, 'remove_dashboard_widgets' ] );
+		\add_action( 'wp_dashboard_setup', [ $this, 'mo_dashboard' ] );
+		\add_action( 'admin_print_styles', [ $this, 'enqueue_assets' ] );
+	}
+
+	/**
+	 * Enqueue plugin assets.
+	 */
+	public function enqueue_assets() {
+		\wp_enqueue_style( 'mo-support', \plugins_url( '/assets/css/mo-support-dashboard-widget.css', __FILE__ ), null, self::PLUGIN_VERSION );
 	}
 
 	/**
@@ -76,10 +94,40 @@ final class Admin_Dashboard {
 		\remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' ); // deprecated.
 		\remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
 		\remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
-		\remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' );
 		\remove_meta_box( 'wpseo-dashboard-overview', 'dashboard', 'normal' ); // Yoast SEO.
 
 		\remove_action( 'welcome_panel', 'wp_welcome_panel' ); // Welcome panel.
+	}
+
+	/**
+	 * Print MONTAGMORGENS support dahsboard widget content.
+	 *
+	 * Add `define( 'MO_SUPPORT', 'active' );` to `wp-config.php` for systems with active support contract
+	 * Add `define( 'MO_SUPPORT', 'free' );` to `wp-config.php` for systems within free support interval
+	 */
+	public function mo_dashboard_content() {
+		$support_contract = defined( 'MO_SUPPORT' ) ? MO_SUPPORT : false;
+
+		if ( 'active' === $support_contract ) {
+			printf(
+				'<p><strong>%s</strong></p><p><span class="mo-support-tag mo-support-tag--active">%s</span></p><p>%s</p><p><hr>',
+				esc_html__( 'Ihr Wartungsvertrag', 'mo-support' ),
+				esc_html__( 'Wartungsvertrag aktiv', 'mo-support' ),
+				esc_html__( 'Wir halten Ihre WordPress-Installation immer auf dem neuesten Stand und legen tägliche Backups an.', 'mo-support' ),
+			);
+		}
+
+		// Print contact info.
+		printf(
+			'<p><strong>%s</strong></p><p>%s</p>',
+			esc_html__( 'WordPress-Notfall?', 'mo-support' ),
+			sprintf(
+				/* translators: %1$s: e-mail-address, %2$s: phone number */
+				esc_html__( 'Bei Fragen zu und Problemen mit Ihrer WordPress-Installation erreichen sie uns per E-Mail unter %1$s oder telefonisch unter %2$s.', 'mo-support' ),
+				'<a href="mailto:support@montagmorgens.com">support@montagmorgens.com</a>',
+				'<a href="tel:+4921515374111">+49 (0)2151 5374-111</a>'
+			)
+		);
 	}
 
 	/**
@@ -90,14 +138,7 @@ final class Admin_Dashboard {
 		\add_meta_box(
 			'mo_support',
 			__( 'MONTAGMORGENS-Support', 'mo-dashboard' ),
-			function() {
-				printf(
-					/* translators: %1$s: e-mail-address, %2$s: phone number */
-					esc_html__( 'Bei Fragen zu und Problemen mit Ihrer WordPress-Installation erreichen sie uns per E-Mail unter %1$s oder telefonisch unter %2$s.', 'mo-support' ),
-					'<a href="mailto:support@montagmorgens.com">support@montagmorgens.com</a>',
-					'<a href="tel:+4921515374111">+49 (0)2151 5374-111</a>'
-				);
-			},
+			[ $this, 'mo_dashboard_content' ],
 			'dashboard',
 			'side',
 			'high'
